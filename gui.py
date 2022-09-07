f245ffbe53647276de15795c230f9549b312b36a
CosineAA <rhdwlgns0725@gmail.com>
2022-09-07 15:00:37 +0900

    update

--- gui.py
+++ gui.py
@@ -1,8 +1,11 @@
 import sys
 import os
+import psutil
+from requests import get
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt5.QtGui import QIcon, QIntValidator
+from PyQt5.QtCore import QSize
 
 form_class = uic.loadUiType("./design.ui")[0]
 
@@ -11,17 +14,21 @@ class MainClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setWindowTitle("EasyBukkit")
         self.setWindowIcon(QIcon("paper.png"))
+
+        self.setFixedSize(QSize(535, 528))
 
         self.only_integer(self.MaxLine)
         self.only_integer(self.MiniLine)
 
-        self.version119.clicked.connect(self.select_version)
+        self.version1192.clicked.connect(self.select_version)
         self.version1171.clicked.connect(self.select_version)
+        self.version1165.clicked.connect(self.select_version)
+        self.version1152.clicked.connect(self.select_version)
+        self.version1144.clicked.connect(self.select_version)
+        self.version1132.clicked.connect(self.select_version)
         self.version1122.clicked.connect(self.select_version)
-        self.version1710.clicked.connect(self.select_version)
-        self.version164.clicked.connect(self.select_version)
-        self.version152.clicked.connect(self.select_version)
 
         self.Start.clicked.connect(self.start_bukkit)
         # self.End.clicked.connect(self.end_bukkit)
@@ -31,7 +38,48 @@ class MainClass(QMainWindow, form_class):
         select.setValidator(QIntValidator(1, 32767, self))
 
     def start_bukkit(self):
+        if self.check_process_running("cmd"):
+            QMessageBox.warning(self, "에러", "이미 버킷을 실행 중입니다.\n(CMD 실행 시, 오류가 발생할 수 있습니다.)")
+            return
+        path = self.PathLine.text()
         self.create_bukkit()
+        self.download_bukkit(self.get_target_bukkit(), f"{path}\\paper.jar")
+        try:
+            os.chdir(path)
+            os.startfile(f"{path}")
+            os.startfile(f"{path}\\Run.bat")
+        except FileNotFoundError as e:
+            print(e)
+
+    def check_process_running(self, process_name):
+        for proc in psutil.process_iter():
+            try:
+                if process_name.lower() in proc.name().lower():
+                    return True
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        return False
+
+    def download_bukkit(self, url, file_name):
+        with open(file_name, "wb") as file:  # open in binary mode
+            response = get(url)  # get request
+            file.write(response.content)
+
+    def get_target_bukkit(self):
+        if self.version1192.isChecked():
+            return "https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/138/downloads/paper-1.19.2-138.jar"
+        if self.version1171.isChecked():
+            return "https://api.papermc.io/v2/projects/paper/versions/1.17.1/builds/411/downloads/paper-1.17.1-411.jar"
+        if self.version1165.isChecked():
+            return "https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar"
+        if self.version1152.isChecked():
+            return "https://api.papermc.io/v2/projects/paper/versions/1.15.2/builds/393/downloads/paper-1.15.2-393.jar"
+        if self.version1144.isChecked():
+            return "https://api.papermc.io/v2/projects/paper/versions/1.14.4/builds/245/downloads/paper-1.14.4-245.jar"
+        if self.version1132.isChecked():
+            return "https://api.papermc.io/v2/projects/paper/versions/1.13.2/builds/657/downloads/paper-1.13.2-657.jar"
+        if self.version1122.isChecked():
+            return "https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar"
 
     def create_bukkit(self):
         path = self.PathLine.text()
@@ -49,9 +97,18 @@ class MainClass(QMainWindow, form_class):
         except FileExistsError as e:
             print(e)
 
-        with open(f"{path}\\Start.bat", "w+") as start:
+        with open(f"{path}\\Run.bat", "w+") as start:
             start.write("@echo off")
             start.write(f"\njava -Xmx{max_ram}{max_byte} -Xms{mini_ram}{mini_byte} -jar paper.jar{self.get_nogui()}")
+            start.write("\npause")
+
+        with open(f"{path}\\eula.txt", "w+", encoding="utf-8") as eula_file:
+            if self.Eula.isChecked():
+                eula = "true"
+            else:
+                eula = "false"
+
+            eula_file.write(f"eula={eula}")
 
     def get_ram(self, select):
         return select.currentText().replace("B", "")
@@ -62,23 +119,26 @@ class MainClass(QMainWindow, form_class):
         return ""
 
     def select_version(self):
-        if self.version119.isChecked():
-            self.PathLine.setText("C:\\EasyBukkit\\1.19")
+        if self.version1192.isChecked():
+            self.PathLine.setText("C:\\EasyBukkit\\1.19.2")
             return
         if self.version1171.isChecked():
             self.PathLine.setText("C:\\EasyBukkit\\1.17.1")
             return
+        if self.version1165.isChecked():
+            self.PathLine.setText("C:\\EasyBukkit\\1.16.5")
+            return
+        if self.version1152.isChecked():
+            self.PathLine.setText("C:\\EasyBukkit\\1.15.2")
+            return
+        if self.version1144.isChecked():
+            self.PathLine.setText("C:\\EasyBukkit\\1.14.4")
+            return
+        if self.version1132.isChecked():
+            self.PathLine.setText("C:\\EasyBukkit\\1.13.2")
+            return
         if self.version1122.isChecked():
             self.PathLine.setText("C:\\EasyBukkit\\1.12.2")
-            return
-        if self.version1710.isChecked():
-            self.PathLine.setText("C:\\EasyBukkit\\1.7.10")
-            return
-        if self.version164.isChecked():
-            self.PathLine.setText("C:\\EasyBukkit\\1.6.4")
-            return
-        if self.version152.isChecked():
-            self.PathLine.setText("C:\\EasyBukkit\\1.5.2")
             return
 
 
@@ -87,7 +147,6 @@ if __name__ == "__main__":
     myWindow = MainClass()
     myWindow.show()
     app.exec_()
-
 
 # import sys
 # from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QGroupBox, QRadioButton, QCheckBox, QPushButton, \
