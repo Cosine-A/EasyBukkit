4beac594f5b63c450276b5f0ae5bb8f3b9ad97ab
CosineAA <rhdwlgns0725@gmail.com>
2022-09-08 10:51:26 +0900

    update

--- easy_bukkit.py
+++ easy_bukkit.py
@@ -17,18 +17,12 @@ class MainClass(QMainWindow, form_class):
         self.setWindowTitle("EasyBukkit")
         self.setWindowIcon(QIcon("paper.png"))
 
-        self.setFixedSize(QSize(535, 528))
+        self.setFixedSize(QSize(500, 400))
 
         self.only_integer(self.MaxLine)
-        self.only_integer(self.MiniLine)
+        self.only_integer(self.MinLine)
 
-        self.version1192.clicked.connect(self.select_version)
-        self.version1171.clicked.connect(self.select_version)
-        self.version1165.clicked.connect(self.select_version)
-        self.version1152.clicked.connect(self.select_version)
-        self.version1144.clicked.connect(self.select_version)
-        self.version1132.clicked.connect(self.select_version)
-        self.version1122.clicked.connect(self.select_version)
+        self.versions.currentTextChanged.connect(self.select_version)
 
         self.Start.clicked.connect(self.start_bukkit)
         # self.End.clicked.connect(self.end_bukkit)
@@ -38,12 +32,22 @@ class MainClass(QMainWindow, form_class):
         select.setValidator(QIntValidator(1, 32767, self))
 
     def start_bukkit(self):
-        if self.check_process_running("cmd"):
-            QMessageBox.warning(self, "에러", "이미 버킷을 실행 중입니다.\n(CMD 실행 시, 오류가 발생할 수 있습니다.)")
-            return
         path = self.PathLine.text()
+
+        if self.versions.currentText() == "버전을 선택해주세요":
+            QMessageBox.warning(self, "에러", "버전을 선택해주세요.")
+            return
+
+        if path == "":
+            QMessageBox.warning(self, "에러", "경로가 설정되어 있지 않습니다.")
+            return
+
+        self.check_process_running("cmd")
         self.create_bukkit()
         self.download_bukkit(self.get_target_bukkit(), f"{path}\\paper.jar")
+        self.run_bukkit(path)
+
+    def run_bukkit(self, path):
         try:
             os.chdir(path)
             os.startfile(f"{path}")
@@ -55,89 +59,98 @@ class MainClass(QMainWindow, form_class):
         for proc in psutil.process_iter():
             try:
                 if process_name.lower() in proc.name().lower():
-                    return True
+                    QMessageBox.warning(self, "에러", "이미 버킷을 실행 중입니다.\n(CMD 실행 시, 오류가 발생할 수 있습니다.)")
+                    return
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                 pass
-        return False
 
     def download_bukkit(self, url, file_name):
         with open(file_name, "wb") as file:  # open in binary mode
             response = get(url)  # get request
             file.write(response.content)
 
-    def get_target_bukkit(self):
-        if self.version1192.isChecked():
-            return "https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/138/downloads/paper-1.19.2-138.jar"
-        if self.version1171.isChecked():
-            return "https://api.papermc.io/v2/projects/paper/versions/1.17.1/builds/411/downloads/paper-1.17.1-411.jar"
-        if self.version1165.isChecked():
-            return "https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar"
-        if self.version1152.isChecked():
-            return "https://api.papermc.io/v2/projects/paper/versions/1.15.2/builds/393/downloads/paper-1.15.2-393.jar"
-        if self.version1144.isChecked():
-            return "https://api.papermc.io/v2/projects/paper/versions/1.14.4/builds/245/downloads/paper-1.14.4-245.jar"
-        if self.version1132.isChecked():
-            return "https://api.papermc.io/v2/projects/paper/versions/1.13.2/builds/657/downloads/paper-1.13.2-657.jar"
-        if self.version1122.isChecked():
-            return "https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar"
-
     def create_bukkit(self):
         path = self.PathLine.text()
         max_ram = self.MaxLine.text()
-        mini_ram = self.MiniLine.text()
+        min_ram = self.MinLine.text()
         max_byte = self.get_ram(self.MaxByte)
-        mini_byte = self.get_ram(self.MiniByte)
+        min_byte = self.get_ram(self.MinByte)
+        nogui = self.get_nogui()
+        eula = self.get_eula()
 
-        if path == "":
-            QMessageBox.warning(self, "에러", "경로가 설정되어 있지 않습니다.")
-            return
+        self.create_folder(path)
+        self.create_starter(path, max_ram, max_byte, min_ram, min_byte, nogui)
+        self.create_eula(path, eula)
 
+    def create_folder(self, path):
         try:
             os.makedirs(path)
         except FileExistsError as e:
             print(e)
 
+    def create_starter(self, path, max_ram, max_byte, min_ram, min_byte, nogui):
         with open(f"{path}\\Run.bat", "w+") as start:
             start.write("@echo off")
-            start.write(f"\njava -Xmx{max_ram}{max_byte} -Xms{mini_ram}{mini_byte} -jar paper.jar{self.get_nogui()}")
+            start.write(f"\njava -Xmx{max_ram}{max_byte} -Xms{min_ram}{min_byte} -jar paper.jar{nogui}")
             start.write("\npause")
 
+    def create_eula(self, path, eula):
         with open(f"{path}\\eula.txt", "w+", encoding="utf-8") as eula_file:
-            if self.Eula.isChecked():
-                eula = "true"
-            else:
-                eula = "false"
-
             eula_file.write(f"eula={eula}")
 
     def get_ram(self, select):
         return select.currentText().replace("B", "")
+
+    def get_eula(self):
+        if self.CheckEula.isChecked():
+            return "true"
+        else:
+            return "false"
 
     def get_nogui(self):
         if self.NoGui.isChecked():
             return " nogui"
         return ""
 
+    def get_target_bukkit(self):
+        if self.versions.currentText() == "1.19.2":
+            return "https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/138/downloads/paper-1.19.2-138.jar"
+        if self.versions.currentText() == "1.17.1":
+            return "https://api.papermc.io/v2/projects/paper/versions/1.17.1/builds/411/downloads/paper-1.17.1-411.jar"
+        if self.versions.currentText() == "1.16.5":
+            return "https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar"
+        if self.versions.currentText() == "1.15.2":
+            return "https://api.papermc.io/v2/projects/paper/versions/1.15.2/builds/393/downloads/paper-1.15.2-393.jar"
+        if self.versions.currentText() == "1.14.4":
+            return "https://api.papermc.io/v2/projects/paper/versions/1.14.4/builds/245/downloads/paper-1.14.4-245.jar"
+        if self.versions.currentText() == "1.13.2":
+            return "https://api.papermc.io/v2/projects/paper/versions/1.13.2/builds/657/downloads/paper-1.13.2-657.jar"
+        if self.versions.currentText() == "1.12.2":
+            return "https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar"
+
     def select_version(self):
-        if self.version1192.isChecked():
+        if self.versions.currentText() == "버전을 선택해주세요":
+            self.PathLine.setText("")
+            return
+        if self.versions.currentText() == "1.19.2":
             self.PathLine.setText("C:\\EasyBukkit\\1.19.2")
             return
-        if self.version1171.isChecked():
+        if self.versions.currentText() == "1.17.1":
             self.PathLine.setText("C:\\EasyBukkit\\1.17.1")
             return
-        if self.version1165.isChecked():
+        if self.versions.currentText() == "1.16.5":
             self.PathLine.setText("C:\\EasyBukkit\\1.16.5")
             return
-        if self.version1152.isChecked():
+        if self.versions.currentText() == "1.15.2":
             self.PathLine.setText("C:\\EasyBukkit\\1.15.2")
             return
-        if self.version1144.isChecked():
+        if self.versions.currentText() == "1.14.4":
             self.PathLine.setText("C:\\EasyBukkit\\1.14.4")
             return
-        if self.version1132.isChecked():
+        if self.versions.currentText() == "1.13.2":
             self.PathLine.setText("C:\\EasyBukkit\\1.13.2")
             return
-        if self.version1122.isChecked():
+        if self.versions.currentText() == "1.12.2":
             self.PathLine.setText("C:\\EasyBukkit\\1.12.2")
             return
 
